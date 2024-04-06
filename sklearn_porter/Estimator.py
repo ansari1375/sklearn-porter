938dca343011be9aab7892a0930c60f53adeed89
ansari1375 <40363213+ansari1375@users.noreply.github.com>
2024-04-06 12:14:30 +0330

    Update Estimator.py

--- sklearn_porter/Estimator.py
+++ sklearn_porter/Estimator.py
@@ -852,13 +852,13 @@ class Estimator:
 
         # scikit-learn version < 0.18.0
         from sklearn.tree.tree import DecisionTreeClassifier
-        from sklearn.ensemble.weight_boosting import AdaBoostClassifier
-        from sklearn.ensemble.forest import RandomForestClassifier
-        from sklearn.ensemble.forest import ExtraTreesClassifier
-        from sklearn.svm.classes import LinearSVC
-        from sklearn.svm.classes import SVC
-        from sklearn.svm.classes import NuSVC
-        from sklearn.neighbors.classification import KNeighborsClassifier
+        from sklearn.ensemble import AdaBoostClassifier
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.ensemble import ExtraTreesClassifier
+        from sklearn.svm import LinearSVC
+        from sklearn.svm import SVC
+        from sklearn.svm import NuSVC
+        from sklearn.neighbors import KNeighborsClassifier
         from sklearn.naive_bayes import GaussianNB
         from sklearn.naive_bayes import BernoulliNB
 
